6a7a1189ff1fee978bc097a64aa73346702d4f8d
blivney <blivney@bu.edu>
2020-09-10 19:51:38 -0400

    Write anomalies to text file

--- processing.py
+++ processing.py
@@ -45,7 +45,7 @@ def printResults(officetemps, lab1temps, class1temps, officeocc, lab1occ, class1
     print("Lab1 Temperature Variance: " + str(numpy.var(lab1temps)))
     print("Class1 Temperature Mean: " + str(numpy.mean(class1temps)))
     print("Class1 Temperature Median: " + str(numpy.median(class1temps)))
-    print("Lab1 Temperature Variance: " + str(numpy.var(class1temps)))
+    print("Class1 Temperature Variance: " + str(numpy.var(class1temps)))
     print("")
     print("Office Occupancy Mean: " + str(numpy.mean(officeocc)))
     print("Office Occupancy Median: " + str(numpy.median(officeocc)))
@@ -55,7 +55,7 @@ def printResults(officetemps, lab1temps, class1temps, officeocc, lab1occ, class1
     print("Lab1 Occupancy Variance: " + str(numpy.var(lab1occ)))
     print("Class1 Occupancy Mean: " + str(numpy.mean(class1occ)))
     print("Class1 Occupancy Median: " + str(numpy.median(class1occ)))
-    print("Lab1 Occupancy Variance: " + str(numpy.var(class1occ)))
+    print("Class1 Occupancy Variance: " + str(numpy.var(class1occ)))
     print("")
     print("Office CO2 Mean: " + str(numpy.mean(officeco2)))
     print("Office CO2 Median: " + str(numpy.median(officeco2)))
@@ -65,7 +65,7 @@ def printResults(officetemps, lab1temps, class1temps, officeocc, lab1occ, class1
     print("Lab1 CO2 Variance: " + str(numpy.var(lab1co2)))
     print("Class1 CO2 Mean: " + str(numpy.mean(class1co2)))
     print("Class1 CO2 Median: " + str(numpy.median(class1co2)))
-    print("Lab1 CO2 Variance: " + str(numpy.var(class1co2)))
+    print("Class1 CO2 Variance: " + str(numpy.var(class1co2)))
 
 
 
@@ -91,8 +91,57 @@ def processData(data):
     class1co2 = removeNaN(data, "co2", "class1")
 
     printResults(officetemps, lab1temps, class1temps, officeocc, lab1occ, class1occ, officeco2, lab1co2, class1co2)
+    anomalyAlgorithm(officetemps, lab1temps, class1temps)
+ 
+# runs through lists to detect what values are in appropriate range
+def anomalyAlgorithm(officetemps, lab1temps, class1temps):
     
+    #means from original data
+    officetempsMean = numpy.mean(officetemps)
+    lab1tempsMean = numpy.mean(lab1temps)
+    class1tempsMean = numpy.mean(class1temps)
+    #std dev from original data
+    officetempsStd = numpy.std(officetemps)
+    lab1tempsStd = numpy.std(lab1temps)
+    class1tempsStd = numpy.std(class1temps)
+    #new list of room temps
+    newofficetemps = [i for i in officetemps if i<(officetempsMean + officetempsStd)  and i>(officetempsMean - officetempsStd)]
+    newlab1temps = [i for i in lab1temps if i<(lab1tempsMean + lab1tempsStd) and i>(lab1tempsMean - lab1tempsStd)]
+    newclass1temps = [i for i in class1temps if i<(class1tempsMean + class1tempsStd) and i>(class1tempsMean - class1tempsStd)]
     
+    officetempanomalies = [i for i in officetemps if i >= (officetempsMean + officetempsStd) or i <=(officetempsMean - officetempsStd)]
+    lab1tempanomalies = [i for i in lab1temps if i >= (lab1tempsMean + lab1tempsStd) or i <= (lab1tempsMean - lab1tempsStd)]
+    class1tempanomalies = [i for i in class1temps if i >= (class1tempsMean + class1tempsStd) or i <= (class1tempsMean - class1tempsStd)]
+    
+    print("Number of data for new office list " + str(len(newofficetemps)))
+    print("Numbeer of data for new lab1 list " + str(len(newlab1temps)))
+    print("Number of data for new class1 list " + str(len(newclass1temps)))
+    print("")
+    print("New office temperature mean " + str(numpy.mean(newofficetemps)))
+    print("New office temperature median " + str(numpy.median(newofficetemps)))
+    print("New office temperature variance " + str(numpy.var(newofficetemps)))
+    print("")
+    print("New lab1 temperature mean " + str(numpy.mean(newlab1temps)))
+    print("New lab1 temperaturee median " + str(numpy.median(newlab1temps)))
+    print("New lab1 temperature variance " + str(numpy.var(newlab1temps)))
+    print("")
+    print("New class1 temperature mean " + str(numpy.mean(newclass1temps)))
+    print("New class1 temperature median " + str(numpy.median(newclass1temps)))
+    print("New class1 temperature variance " + str(numpy.var(newclass1temps)))
+    print("")
+    print('Anomalous data written to "anomalies.txt"')
+    file = open("anomalies.txt", mode = "w")
+    file.write("Office:\n")
+    for l in officetempanomalies:
+        file.write(str(l) + "\n")
+    file.write("Lab1:\n")
+    for l in lab1tempanomalies:
+        file.write(str(l) + "\n")
+    file.write("Class1:\n")
+    for l in class1tempanomalies:
+        file.write(str(l) + "\n")
+    file.close()
+        
 
 def plotData(data, sensor, room, col, x_label):
     points = []
